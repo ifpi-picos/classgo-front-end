7cef355aea83f1345aae1d65a66ca0a22430fddb
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-13 18:11:27 -0300

    ajustes

--- src/app/redefinepassword/page.jsx
+++ src/app/redefinepassword/page.jsx
@@ -1,6 +1,6 @@
 import RedefinePasswordForm from "@/components/RedefinePasswordForm"
 
-export default function RedefinePassowordPage() {
+export default function RedefinePasswordPage() {
     return (
         <main className="h-screen w-full flex items-center justify-center">
             <RedefinePasswordForm/>
