285bc44ef7295653a3deb8141f2d02f29349a5ba
PamellaTharlla <pamellatharlla90@gmail.com>
2023-11-23 01:20:09 -0300

    atualização

--- src/components/UserFomr.jsx
+++ src/components/UserFomr.jsx
@@ -1,13 +1,17 @@
 export default function Teladeperfil() {
     return (
         <div className= "flex items-center justify-center h-screen"> 
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className ="bi bi-person-fill" viewBox="0 0 16 16">
-  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-</svg>
+        
+     
            
            <form action="entar" className="bg-white p-8 rounded shadow-md w-96">
+            <div className="flex items-start justify-center mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg"  width="85" height="55"  fill="currentColor" className =" bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg>
+            </div>
            <label htmlFor="em"> Email </label>
-            <input type="email "   id="email" className="block w-full mt-1 mb-4 p-2 border rounded"/> 
+            <input type="email "  placeholder="Email"  id="email" className="block w-full mt-1 mb-4 p-2 border rounded"/> 
              <br />
              <label htmlFor="se"> senha</label>
             <input type="senha"  placeholder="Senha" id="senha" className="block w-full mt-1 mb-4 p-2 border rounded">
