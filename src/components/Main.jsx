17a0f0c8856c3ea7d94fd6e71d67f1749856ef93
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-25 14:07:24 -0300

    criação do componente Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+export default function Main() {
+    return (
+        <main className="w-4/5 h-screen flex justify-center items-center">
+            <span>Main</span>
+        </main>
+    )
+}
