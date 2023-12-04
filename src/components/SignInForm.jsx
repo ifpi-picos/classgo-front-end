8f4d12464d9029ddedeae4359cfb00acb3fa7703
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-12-04 01:20:07 -0300

    responsividade

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -46,14 +46,14 @@ export default function SignInForm() {
 
     return (
         <div className="flex justify-center items-center w-full h-screen bg-blue-50">
-            <form className="flex justify-center items-center w-1/3 bg-blue-500 rounded-xl shadow-md max-sm:w-11/12 max-md:w-3/4 max-lg:w-3/5 max-xl:w-1/2 max-2xl:w-2/5" onSubmit={signIn}>
-                <fieldset className="flex flex-col items-center w-5/6 my-10 border border-gray-50 rounded-xl max-sm:w-full max-sm:m-5 max-sm:text-xs">
-                    <div className="flex justify-center items-center my-16 text-gray-50 text-xl max-sm:mb-8 max-sm:text-sm">
+            <form className="flex justify-center items-center w-1/3 bg-blue-500 rounded-xl shadow-md" onSubmit={signIn}>
+                <fieldset className="flex flex-col items-center w-5/6 my-10 border border-gray-50 rounded-xl">
+                    <div className="flex justify-center items-center my-16 text-gray-50 text-xl">
                         <span>Login</span>
                     </div>
 
-                    <div className="flex flex-col items-center w-5/6 my-5 max-sm:w-11/12 max-sm:mb-2">
-                        <div className="flex justify-center items-center w-full mb-5 max-sm:mb-4">
+                    <div className="flex flex-col items-center w-5/6 my-5">
+                        <div className="flex justify-center items-center w-full mb-5">
                             <input
                                 className="w-full px-3 py-2 rounded-xl"
                                 id="email"
@@ -85,13 +85,13 @@ export default function SignInForm() {
                         </div>
                     </div>
 
-                    <div className="flex justify-end w-5/6 max-sm:w-11/12">
+                    <div className="flex justify-end w-5/6">
                             <Link className="underline text-gray-50" href="/forgotpassword">
                                 esqueceu senha?
                             </Link>
                     </div>
 
-                    <div className="flex flex-col items-center my-10 text-gray-50 max-sm:my-8">
+                    <div className="flex flex-col items-center my-10 text-gray-50">
                         <button className="mb-5 px-6 py-3 border border-gray-50 rounded-xl" disabled={signInButtonDisabled}>
                             Entrar
                         </button>
--- src/components/VisiblePassword.jsx
+++ src/components/VisiblePassword.jsx
@@ -8,12 +8,12 @@ export default function VisiblePassword({visiblePassoword, setVisiblePassword}) 
             <div className="w-12 h-8 absolute flex justify-center items-center">
                 {!visiblePassoword ? (
                     <HiEye
-                        className="text-gray-500 text-xl absolute cursor-pointer max-sm:text-base"
+                        className="text-gray-500 text-xl absolute cursor-pointer"
                         onClick={() => setVisiblePassword(true)}
                     />
                 ): (
                     <HiEyeOff
-                        className="text-gray-500 text-xl absolute cursor-pointer max-sm:text-base"
+                        className="text-gray-500 text-xl absolute cursor-pointer"
                         onClick={() => setVisiblePassword(false)}
                     />
                 )}
