0bf9ee19b60fd103edae5e34558ef0cacd727065
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-12-04 01:08:57 -0300

    responsividade

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -47,13 +47,13 @@ export default function SignInForm() {
     return (
         <div className="flex justify-center items-center w-full h-screen bg-blue-50">
             <form className="flex justify-center items-center w-1/3 bg-blue-500 rounded-xl shadow-md max-sm:w-11/12 max-md:w-3/4 max-lg:w-3/5 max-xl:w-1/2 max-2xl:w-2/5" onSubmit={signIn}>
-                <fieldset className="flex flex-col items-center w-5/6 my-10 border border-gray-50 rounded-xl max-sm:my-6 max-sm:text-xs">
-                    <div className="flex justify-center items-center my-16 text-gray-50 text-xl max-sm:my-12 max-sm:text-sm">
+                <fieldset className="flex flex-col items-center w-5/6 my-10 border border-gray-50 rounded-xl max-sm:w-full max-sm:m-5 max-sm:text-xs">
+                    <div className="flex justify-center items-center my-16 text-gray-50 text-xl max-sm:mb-8 max-sm:text-sm">
                         <span>Login</span>
                     </div>
 
-                    <div className="flex flex-col items-center w-5/6 my-5 max-sm:w-11/12">
-                        <div className="flex justify-center items-center w-full mb-5">
+                    <div className="flex flex-col items-center w-5/6 my-5 max-sm:w-11/12 max-sm:mb-2">
+                        <div className="flex justify-center items-center w-full mb-5 max-sm:mb-4">
                             <input
                                 className="w-full px-3 py-2 rounded-xl"
                                 id="email"
@@ -91,7 +91,7 @@ export default function SignInForm() {
                             </Link>
                     </div>
 
-                    <div className="flex flex-col items-center my-10 text-gray-50">
+                    <div className="flex flex-col items-center my-10 text-gray-50 max-sm:my-8">
                         <button className="mb-5 px-6 py-3 border border-gray-50 rounded-xl" disabled={signInButtonDisabled}>
                             Entrar
                         </button>
