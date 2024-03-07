02c7416918f278a5138713129640a697f069f485
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-06 21:17:52 -0300

    ajustes

--- src/components/user/RedefinePasswordForm.jsx
+++ src/components/user/RedefinePasswordForm.jsx
@@ -66,7 +66,7 @@ export default function RedefinePasswordForm({id}) {
                         </div>
 
                         <div className="flex flex-col items-center w-5/6 my-5">
-                            <div className="w-full flex flex-col items-center l mb-5">
+                            <div className="flex justify-center items-center w-full mb-5">
                                 <input
                                     className="w-full pl-3 pr-12 py-2 rounded-xl"
                                     id="newPassword"
@@ -85,7 +85,7 @@ export default function RedefinePasswordForm({id}) {
                                 />
                             </div>
 
-                            <div className="flex flex-col items-center w-full">
+                            <div className="flex justify-center items-center w-full">
                                 <input
                                     className="w-full pl-3 pr-12 py-2 rounded-xl"
                                     id="confirmNewPassword"
