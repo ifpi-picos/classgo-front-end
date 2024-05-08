4557c279701e1d06f066824bb4e2e8d794083e20
landeilsonveloso <landeilsonveloso@gmail.com>
2024-05-08 11:51:03 -0300

    rota signup

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,5 @@
+import SignUp from "@/components/users/SignUp"
+
+export default function SignUpPage() {
+    return <SignUp/>
+}
--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -3,25 +3,56 @@ import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function useUser() {
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [signInButtonDisabled, setSignInButtonDisabled] = useState(false)
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false)
 
     const router = useRouter()
 
+    const signUpUrl = `https://idcurso-back-end.vercel.app/users`
     const signInUrl = `https://idcurso-back-end.vercel.app/users/signin`
+
+    const signUp = (e) => {
+        e.preventDefault()
+
+        if (password !== confirmPassword) {
+            return alert("Campo senha e confirmar senha distintos!")
+        }
+
+        setSubmitButtonDisabled(true)
+
+        axios
+            .post(signUpUrl, {name, email, password, confirmPassword})
+            .then((res) => {
+                if (res.status === 201) {
+                    alert(res.data)
+                    setSubmitButtonDisabled(false)
+                    router.push("/")
+                    return
+                }
+            })
+            .catch((err) => {
+                if (err.response.status === 400) {
+                    alert(err.response.data)
+                    setSubmitButtonDisabled(false)
+                    return
+                }
+            })
+    }
 
     const signIn = (e) => {
         e.preventDefault()
 
-        setSignInButtonDisabled(true)
+        setSubmitButtonDisabled(true)
 
         axios
             .post(signInUrl, {email, password})
             .then((res) => {
                 if (res.status === 200) {
                     localStorage.setItem("token", res.data)
-                    setSignInButtonDisabled(false)
+                    setSubmitButtonDisabled(false)
                     router.replace("/myclasses")
                     return
                 }
@@ -29,16 +60,19 @@ export default function useUser() {
             .catch((err) => {
                 if (err.response.status === 400) {
                     alert(err.response.data)
-                    setSignInButtonDisabled(false)
+                    setSubmitButtonDisabled(false)
                     return
                 }
             })
     }
 
     return {
+        setName,
         setEmail,
         setPassword,
+        setConfirmPassword,
+        signUp,
         signIn,
-        signInButtonDisabled
+        submitButtonDisabled
     }
 }
