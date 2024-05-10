8a4aafe5d9d734b68e0c14f8e372e3352145c007
landeilsonveloso <landeilsonveloso@gmail.com>
2024-05-10 12:33:48 -0300

    ajustes

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
+import { useCallback, useState } from "react"
 import { useRouter } from "next/navigation"
-import { useState } from "react"
 
 export default function useUser() {
     const [name, setName] = useState("")
@@ -16,7 +16,7 @@ export default function useUser() {
     const forgotPasswordUrl = `https://idcurso-back-end.vercel.app/users/forgotpassword`
     const redefinePasswordUrl = `https://idcurso-back-end.vercel.app/users/redefinepassword`
 
-    const signUp = (e) => {
+    const signUp = useCallback(async (e) => {
         e.preventDefault()
 
         if (password !== confirmPassword) {
@@ -25,110 +25,110 @@ export default function useUser() {
 
         setSubmitButtonDisabled(true)
 
-        axios
-            .post(signUpUrl, {name, email, password, confirmPassword})
-            .then((res) => {
-                if (res.status === 201) {
-                    alert(res.data)
-                    router.push("/")
-                    return
-                }
-            })
-            .catch((err) => {
-                if (err.response.status === 400) {
-                    alert(err.response.data)
-                    setSubmitButtonDisabled(false)
-                    return
-                }
-            })
-    }
+        await axios
+                    .post(signUpUrl, {name, email, password, confirmPassword})
+                    .then((res) => {
+                        if (res.status === 201) {
+                            alert(res.data)
+                            router.push("/")
+                            return
+                        }
+                    })
+                    .catch((err) => {
+                        if (err.response.status === 400) {
+                            alert(err.response.data)
+                            setSubmitButtonDisabled(false)
+                            return
+                        }
+                    })
+    }, [name, email, password, confirmPassword])
 
-    const signIn = (e) => {
+    const signIn = useCallback(async (e) => {
         e.preventDefault()
 
         setSubmitButtonDisabled(true)
 
-        axios
-            .post(signInUrl, {email, password})
-            .then((res) => {
-                if (res.status === 200) {
-                    localStorage.setItem("token", res.data)
-                    router.replace("/myclasses")
-                    return
-                }
-            })
-            .catch((err) => {
-                if (err.response.status === 400) {
-                    alert(err.response.data)
-                    setSubmitButtonDisabled(false)
-                    return
-                }
-            })
-    }
+        await axios
+                    .post(signInUrl, {email, password})
+                    .then((res) => {
+                        if (res.status === 200) {
+                            localStorage.setItem("token", res.data)
+                            router.replace("/myclasses")
+                            return
+                        }
+                    })
+                    .catch((err) => {
+                        if (err.response.status === 400) {
+                            alert(err.response.data)
+                            setSubmitButtonDisabled(false)
+                            return
+                        }
+                    })
+    }, [email, password])
 
-    const forgotPassword = (e) => {
+    const forgotPassword = useCallback(async (e) => {
         e.preventDefault()
 
         setSubmitButtonDisabled(true)
 
-        axios
-            .post(forgotPasswordUrl, {email})
-            .then((res) => {
-                if (res.status === 200) {
-                    alert("Pedido de solicitação enviado para seu email!")
-                    localStorage.setItem("token", res.data)
-                    setSubmitButtonDisabled(false)
-                    return
-                }
-            })
-            .catch((err) => {
-                if (err.response.status === 400) {
-                    alert(err.response.data)
-                    setSubmitButtonDisabled(false)
-                    return
-                }
-            })
-    }
+        await axios
+                    .post(forgotPasswordUrl, {email})
+                    .then((res) => {
+                        if (res.status === 200) {
+                            alert("Pedido de solicitação enviado para seu email!")
+                            localStorage.setItem("token", res.data)
+                            setSubmitButtonDisabled(false)
+                            return
+                        }
+                    })
+                    .catch((err) => {
+                        if (err.response.status === 400) {
+                            alert(err.response.data)
+                            setSubmitButtonDisabled(false)
+                            return
+                        }
+                    })
+    }, [email])
 
-    const redefinePassword = (e) => {
+    const redefinePassword = useCallback(async (e) => {
         e.preventDefault()
 
         setSubmitButtonDisabled(true)
 
-        axios
-            .put(redefinePasswordUrl, {password, confirmPassword}, {headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": localStorage.getItem("token")
-            }})
-            .then((res) => {
-                if (res.status === 200) {
-                    alert(res.data)
-                    localStorage.clear()
-                    router.replace("/")
-                    return
-                }
+        await axios
+                    .put(redefinePasswordUrl, {password, confirmPassword}, {headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json",
+                        "Authorization": localStorage.getItem("token")
+                    }})
+                    .then((res) => {
+                        if (res.status === 200) {
+                            alert(res.data)
+                            localStorage.clear()
+                            router.replace("/")
+                            return
+                        }
 
-                else if (res.status === 401) {
-                    localStorage.clear()
-                    router.replace("/")
-                    return
-                }
-            })
-            .catch((err) => {
-                if (err.response.status === 400) {
-                    alert(err.response.data)
-                    setSubmitButtonDisabled(false)
-                    return
-                }
+                        else if (res.status === 401) {
+                            localStorage.clear()
+                            router.replace("/")
+                            return
+                        }
+                    })
+                    .catch((err) => {
+                        if (err.response.status === 400) {
+                            alert(err.response.data)
+                            setSubmitButtonDisabled(false)
+                            return
+                        }
 
-                else if (err.response.status === 401) {
-                    localStorage.clear()
-                    router.replace("/")
-                    return
-                }
-            })
-    }
+                        else if (err.response.status === 401) {
+                            localStorage.clear()
+                            router.replace("/")
+                            return
+                        }
+                    })
+    }, [password, confirmPassword])
 
     return {
         setName,
