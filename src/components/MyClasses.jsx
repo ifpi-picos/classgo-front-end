f8271b3de320b046060354a21c950239ec6c3bb0
landeilsonveloso <landeilsonveloso@gmail.com>
2024-05-15 21:54:25 -0300

    responsividade

--- src/components/MyClasses.jsx
+++ src/components/MyClasses.jsx
@@ -1,23 +1,31 @@
+"use client"
+
 import FirstClassImg from "../../assets/FirstClassImg.jpg"
 import Image from "next/image"
 import SideBar from "./SideBar"
+import { HiMenu, HiX } from "react-icons/hi"
+import { useState } from "react"
 
 export default function MyClasses() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className="flex justify-end w-full h-screen select-none">
-            <SideBar/>
+            <SideBar isOpen={isOpen}/>
 
-            <main className="flex flex-col w-4/5 lg:w-[70%] sm:w-[80%]">
-                <header className="flex justify-center items-center w-full h-32 text-neutral-800 text-xl shadow-md md:text-base xs:text-sm">
+            <main className="flex flex-col w-4/5 xs:w-full">
+                <header className="flex justify-center items-center w-full h-32 text-neutral-800 text-xl shadow-md">
+                    <HiMenu className={`hidden absolute left-8 text-2xl cursor-pointer ${isOpen ? "xs:hidden" : "xs:flex"}`} onClick={() => setIsOpen(!false)}/>
+                    <HiX className={`hidden absolute left-8 text-2xl cursor-pointer text-white ${isOpen ? "xs:z-0 xs:flex" : "xs:hidden"}`} onClick={() => setIsOpen(false)}/>
                     <span>Minhas Turmas</span>
                 </header>
 
                 <section className="flex flex-grow flex-col justify-center items-center w-full">
-                    <div className="w-1/4 lg:w-1/2 sm:w-3/5 xs:w-4/5">
+                    <div className="w-1/4 lg:w-1/2">
                         <Image className="w-full" src={FirstClassImg} alt="Imagem ilustrativa" priority/>
                     </div>
 
-                    <button className="px-4 py-2 bg-green-500 text-white font-semibold shadow-md rounded-xl md:text-sm xs:text-xs xs:px-8" type="button">
+                    <button className="px-4 py-2 bg-green-500 text-white font-semibold shadow-md rounded-xl" type="button">
                         <span>Crie uma Turma</span>
                     </button>
                 </section>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,29 +1,28 @@
 "use client"
 
-import { HiAcademicCap, HiMenu, HiOutlineLogin, HiUser } from "react-icons/hi"
+import { HiAcademicCap, HiOutlineLogin, HiUser } from "react-icons/hi"
 import Link from "next/link"
 
-export default function SideBar() {
+export default function SideBar({isOpen}) {
     return (
-        <nav className="flex flex-col items-center fixed left-0 float-left overflow-auto w-1/5 h-screen bg-blue-500 text-white shadow-md lg:w-[30%] md:text-sm sm:w-[20%]">
-            <div className="flex justify-center items-center w-full h-32">
-                <HiMenu className="hidden text-2xl sm:block"/>
-                <span className="sm:hidden">idCurso</span>
+        <nav className={`flex flex-col items-center fixed left-0 float-left overflow-auto w-1/5 h-screen bg-blue-500 text-white shadow-md ${isOpen ? "xs:z-0 xs:w-4/5" : "xs:hidden"}`}>
+            <div className="flex justify-center items-center w-full h-32 p-2">
+                <span>idCurso</span>
             </div>
 
-            <Link className="flex items-center w-[90%] mb-4 p-2 rounded-xl hover:bg-blue-400 active:bg-blue-500 sm:justify-center" href="/myclasses">
+            <Link className="flex items-center w-[90%] mb-4 p-2 rounded-xl hover:bg-blue-400 active:bg-blue-500" href="/myclasses">
                 <HiAcademicCap className="text-2xl"/>
-                <span className="ml-4 sm:hidden">Turmas</span>
+                <span className="ml-4">Turmas</span>
             </Link>
 
-            <Link className="flex items-center w-[90%] mb-4 p-2 rounded-xl hover:bg-blue-400 active:bg-blue-500 sm:justify-center" href="/profile">
+            <Link className="flex items-center w-[90%] mb-4 p-2 rounded-xl hover:bg-blue-400 active:bg-blue-500" href="/profile">
                 <HiUser className="text-2xl"/>
-                <span className="ml-4 sm:hidden">Perfil</span>
+                <span className="ml-4">Perfil</span>
             </Link>
 
-            <Link className="flex items-center w-[90%] p-2 rounded-xl hover:bg-red-400 active:bg-red-500 sm:justify-center" href="/">
+            <Link className="flex items-center w-[90%] p-2 rounded-xl hover:bg-red-400 active:bg-red-500" href="/">
                 <HiOutlineLogin className="text-2xl"/>
-                <span className="ml-4 sm:hidden">Sair</span>
+                <span className="ml-4">Sair</span>
             </Link>
         </nav>
     )
