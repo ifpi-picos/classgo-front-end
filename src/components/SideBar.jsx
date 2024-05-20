aa4716c91f008fe3b0e258abc15d40a7ca3717eb
landeilsonveloso <landeilsonveloso@gmail.com>
2024-05-20 17:54:52 -0300

    ajustes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,7 +9,7 @@ export default function SideBar() {
 
     return (
         <>
-            <nav className={`flex flex-col items-center fixed left-0 float-left overflow-auto w-1/5 h-screen bg-blue-500 text-white shadow-md xs:${isOpen ? "w-4/5 z-50" : "hidden"}`}>
+            <nav className={`flex flex-col items-center fixed left-0 float-left overflow-auto w-1/5 h-screen bg-blue-500 text-white shadow-md ${isOpen ? "xs:w-4/5 xs:z-50" : "xs:hidden"}`}>
                 <div className="flex justify-center items-center w-full h-32 p-2 xs:w-[90%] xs:justify-between">
                     <span>idCurso</span>
                     <HiX className="hidden text-2xl xs:flex" onClick={disableSideBar}/>
@@ -31,7 +31,7 @@ export default function SideBar() {
                 </Link>
             </nav>
 
-            <HiMenu className={`fixed hidden z-50 left-8 top-9 text-2xl xs:${isOpen ? "hidden" : "flex"}`} onClick={enableSideBar}/>
+            <HiMenu className={`fixed hidden z-50 left-8 top-9 text-2xl ${isOpen ? "xs:hidden" : "xs:flex"}`} onClick={enableSideBar}/>
         </>
     )
 }
